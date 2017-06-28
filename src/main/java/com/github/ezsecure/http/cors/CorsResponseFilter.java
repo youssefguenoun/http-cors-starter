b419e0b28953129233a51be5f2f24b8ef112bf1d
Youssef Guenoun <youssef.guenoun@antelop.fr>
2017-06-28 17:47:18 +0200

    Spring boot starter to enable cors filter

---
+++ src/main/java/com/github/ezsecure/http/cors/CorsResponseFilter.java
@@ -0,0 +1,60 @@
+package com.github.ezsecure.http.cors;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by youssefguenoun on 28/06/2017.
+ */
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsResponseFilter implements Filter {
+
+    private @Value("${http.cors.allow.credentials:true}") boolean allowCredentials;
+    private @Value("${http.cors.allow.headers:Origin}") String allowHeaders;
+    private @Value("${http.cors.allow.methods:GET, POST, DELETE, OPTIONS, HEAD, PATCH}") String allowMethods;
+    private @Value("${http.cors.allow.origin:*}") String allowOrigin;
+    private @Value("${http.cors.max.age:1209600}") String maxAge;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        response.setHeader("Access-Control-Allow-Credentials", Boolean.toString(allowCredentials));
+        response.setHeader("Access-Control-Allow-Headers", allowHeaders);
+        response.setHeader("Access-Control-Allow-Methods", allowMethods);
+        response.setHeader("Access-Control-Allow-Origin", allowOrigin);
+        response.setHeader("Access-Control-Max-Age", maxAge);
+
+        if(allowCredentials){
+            String origin = request.getHeader(HttpHeaders.ORIGIN);
+            response.setHeader("Access-Control-Allow-Origin", origin);
+        }
+
+        if(HttpMethod.OPTIONS.name().equalsIgnoreCase(request.getMethod())){
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
